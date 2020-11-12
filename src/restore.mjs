5bf0169328fd62b87c476b858331384769370f7b
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-12 23:42:25 +0300

    git: add src/restore.mjs core

---
+++ src/restore.mjs
@@ -0,0 +1,18 @@
+function restore(propsArray, origin, backup) {
+
+	for(let i = 0; i < propsArray.length; ++i) {
+
+		const el = propsArray[i];
+
+		if(backup[el]) {
+
+			origin[el] = backup[el];
+			backup[el] = null;
+
+		}
+
+	}
+
+}
+
+export default restore;
