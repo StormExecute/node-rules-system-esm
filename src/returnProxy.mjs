6cbef77b3af30cf0308f630bf45e2d433dd198db
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-12 23:42:44 +0300

    git: add src/returnProxy.mjs core

---
+++ src/returnProxy.mjs
@@ -0,0 +1,35 @@
+const get = (_, prop) => {
+
+	if(prop == Symbol.toPrimitive) {
+
+		return () => "0"
+
+	} else if(prop == "then") {
+
+		return awaitProxy;
+
+	} else {
+
+		return returnProxy;
+
+	}
+
+}
+
+const awaitProxy = new Proxy({}, {
+
+	get,
+
+})
+
+const returnProxy = new Proxy(class {}, {
+
+	get,
+	has: () => returnProxy,
+	deleteProperty: () => returnProxy,
+	apply: () => returnProxy,
+	construct: () => returnProxy,
+
+});
+
+export default returnProxy;
