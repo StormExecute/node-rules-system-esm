0f2ecfa8e307acd67501b5db685400916e89c970
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-13 23:55:54 +0300

    git: add src/fullSecure.mjs core

---
+++ src/fullSecure.mjs
@@ -0,0 +1,63 @@
+import { password, needToSetPassword, wrongPass } from "./password.mjs";
+
+import { wrongPassEmitter } from "./logs.mjs";
+
+export default function makeFullSecure(
+	connections, fs,
+	process, child_process, dgram, worker_threads, cluster
+) {
+
+	function fullSecure(tryPass, status) {
+
+		status = status || "enable";
+
+		if(password.value === null) throw new Error(needToSetPassword);
+		if(tryPass != password.value) return wrongPassEmitter(wrongPass, "fullSecure", { status });
+
+		return status == "enable" ? [
+
+			connections.block(tryPass),
+			fs.block(tryPass),
+
+			process.block(tryPass),
+
+			child_process.block(tryPass),
+			dgram.block(tryPass),
+			worker_threads.block(tryPass),
+			cluster.block(tryPass),
+
+		] : [
+
+			connections.allow(tryPass),
+			fs.allow(tryPass),
+
+			process.allow(tryPass),
+
+			child_process.allow(tryPass),
+			dgram.allow(tryPass),
+			worker_threads.allow(tryPass),
+			cluster.allow(tryPass),
+
+		];
+
+	}
+
+	return {
+
+		fullSecure,
+
+		enableFullSecure: function (tryPass) {
+
+			return fullSecure(tryPass, "enable");
+
+		},
+
+		disableFullSecure: function (tryPass) {
+
+			return fullSecure(tryPass, "disable");
+
+		},
+
+	}
+
+};
