0d225eea3b2293afb9b24690d5b4a9cba95f33ce
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-13 23:52:36 +0300

    git: add src/process/ core

---
+++ src/process/blockBindings.mjs
@@ -0,0 +1,157 @@
+import { password, needToSetPassword, wrongPass } from "../password.mjs";
+
+import getCallerPaths from "../getCallerPaths.mjs";
+
+import returnProxy from "../returnProxy.mjs";
+
+import isObject from "../../dependencies/isObject.mjs";
+
+import { logsEmitter, wrongPassEmitter } from "../logs.mjs";
+
+import {
+
+	addFullPathToWhiteList,
+	addProjectPathToWhiteList,
+	addDependencyToWhiteList,
+	addDependencyPathToWhiteList,
+
+} from "../whiteListFunctionality.mjs";
+
+import $process from "./storeBindings.mjs";
+
+const block = {};
+
+["binding", "_linkedBinding", "dlopen"].forEach(el => {
+
+	//{ returnProxyInsteadThrow, whiteList, whiteListType }
+	block[el] = function (tryPass, options) {
+
+		if(password.value === null) throw new Error(needToSetPassword);
+		if(tryPass != password.value) {
+
+			const where = el == "binding" ? "Binding" : el == "_linkedBinding" ? "LinkedBinding" : "Dlopen";
+
+			return wrongPassEmitter(wrongPass, "blockProcess" + where, { options })
+
+		}
+
+		if(el == "binding" && $process.statusBinding == true) return false;
+		if(el == "_linkedBinding" && $process.statusLinkedBinding == true) return false;
+		if(el == "dlopen" && $process.statusDlopen == true) return false;
+
+		const opts = isObject(options) ? Object.assign({}, options) : {};
+
+		const whiteList = [];
+		whiteList.name = el;
+
+		if(typeof opts["whiteListType"] == "string" && Array.isArray(opts.whiteList)) {
+
+			if (opts["whiteListType"] == "full") {
+
+				addFullPathToWhiteList(whiteList, tryPass, opts.whiteList);
+
+			} else if (opts["whiteListType"] == "dependency") {
+
+				addDependencyToWhiteList(whiteList, tryPass, opts.whiteList);
+
+			} else if (opts["whiteListType"] == "dependencyPath") {
+
+				addDependencyPathToWhiteList(whiteList, tryPass, opts.whiteList);
+
+			} else {
+
+				addProjectPathToWhiteList(whiteList, tryPass, opts.whiteList);
+
+			}
+
+		}
+
+		const blockedReturn = function (module, nativePath, wrapPath) {
+
+			nativePath = nativePath || "";
+			wrapPath = wrapPath || "";
+
+			logsEmitter("callFn", [nativePath || undefined, wrapPath || undefined], {
+
+				grantRights: false,
+
+				fn: el,
+				args: module,
+
+				calledAsClass: null,
+
+			});
+
+			if(typeof opts["returnProxyInsteadThrow"] == "boolean") return returnProxy;
+
+			throw new Error("[node-rules-system] The script does not have access to process." + el + "!\n"
+				+ "NativePath: " + nativePath + "\n"
+				+ "WrapPath: " + wrapPath) + "\n";
+
+		}
+
+		$process[el] = process[el];
+
+		process[el] = function (module) {
+
+			const callerPaths = getCallerPaths();
+
+			if (!callerPaths) return blockedReturn(module);
+
+			const [nativePath, wrapPath] = callerPaths;
+
+			for (let i = 0; i < whiteList.length; ++i) {
+
+				if (
+					nativePath.startsWith(whiteList[i][0])
+					&&
+					wrapPath.startsWith(whiteList[i][1])
+				) {
+
+					logsEmitter("callFn", [nativePath, wrapPath], {
+
+						grantRights: true,
+
+						fn: el,
+						args: module,
+
+						calledAsClass: null,
+
+					});
+
+					return $process[el](module);
+
+				}
+
+			}
+
+			return blockedReturn(module, nativePath, wrapPath);
+
+		};
+
+		if(el == "binding") $process.statusBinding = true;
+		else if(el == "_linkedBinding") $process.statusLinkedBinding = true;
+		else if(el == "dlopen") $process.statusDlopen = true;
+
+		return true;
+
+	};
+
+});
+
+block.blockAll = function (tryPass, options) {
+
+	if(password.value === null) throw new Error(needToSetPassword);
+	if(tryPass != password.value) return wrongPassEmitter(wrongPass, "blockProcessBindingLinkedBindingAndDlopen");
+
+	return [
+
+		block["binding"](tryPass, options),
+		block["_linkedBinding"](tryPass, options),
+		block["dlopen"](tryPass, options),
+
+	];
+
+};
+
+export default block;
---
+++ src/process/storeBindings.mjs
@@ -0,0 +1,11 @@
+export default {
+
+	statusBinding: false,
+	statusLinkedBinding: false,
+	statusDlopen: false,
+
+	binding: null,
+	_linkedBinding: null,
+	dlopen: null,
+
+};
