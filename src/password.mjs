ce1151d3c51582f973b1977c832bafdd0bab71e4
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-13 23:53:45 +0300

    git: add src/password.mjs core

---
+++ src/password.mjs
@@ -0,0 +1,76 @@
+const prefix = "[node-rules-system]";
+
+const needToSetPassword = prefix + " To manage rules, you need to set a password!";
+const passAlready = prefix + " The password is already set!";
+
+const wrongPass = prefix + " Wrong password!";
+const wrongLastPass = prefix + " Wrong last password!";
+
+const mustBeString = prefix + " The password must be a string!";
+
+const password = { value: null };
+
+import { logsEmitter } from "./logs.mjs";
+
+function setPassword (newPassword) {
+
+	if(typeof newPassword != "string") throw new Error(mustBeString);
+
+	if(password.value === null) {
+
+		password.value = newPassword;
+
+		logsEmitter.force("setPassword", null, { where: "init" });
+
+		return true;
+
+	} else {
+
+		logsEmitter("passwordAlready");
+
+		throw new Error(passAlready);
+
+	}
+
+}
+
+function changePassword(lastPassword, newPassword) {
+
+	if(typeof lastPassword != "string" || typeof newPassword != "string") throw new Error(mustBeString);
+
+	if(password.value === null) return setPassword(newPassword);
+
+	if(lastPassword == password.value) {
+
+		password.value = newPassword;
+
+		logsEmitter("changePassword");
+
+		return true;
+
+	} else {
+
+		logsEmitter("wrongChangePassword", null, { wrongLastPass: lastPassword });
+
+		throw new Error(wrongLastPass);
+
+	}
+
+}
+
+export {
+
+	password,
+
+	needToSetPassword,
+	passAlready,
+
+	wrongPass,
+	wrongLastPass,
+
+	mustBeString,
+
+	setPassword,
+	changePassword,
+
+};
